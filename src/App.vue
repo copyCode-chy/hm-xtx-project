962a35196cdb8ac77b572b1f4e64b7ea5e1cc011
陈航宇zz <2393359544@qq.com>
2025-05-14 14:15:30 +0800

    7.scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <!-- 路由出口 -->
   <router-view></router-view>
+  <div class="aa">
+    ssssss
+  </div>
 </template>
 
 <script setup>
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.aa {
+  color: $priceColor;
+}
+</style>

5cede53425f11cd05c869302868dae2903f38adf
陈航宇zz <2393359544@qq.com>
2025-05-23 11:47:22 +0800

    33.图片预览，小图切大图

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -4,6 +4,7 @@ import { getDetail } from "@/apis/detail";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import detailHot from "./components/detailHot.vue";
+import imageView from "@/components/imageView/index.vue"
 
 const goodsData = ref({});
 const route = useRoute();
@@ -42,7 +43,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <imageView></imageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>

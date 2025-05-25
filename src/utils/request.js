507accdf7b08e7be5ff152b9aea380606f95a7cd
陈航宇zz <2393359544@qq.com>
2025-05-25 12:25:30 +0800

    46.token失效报错问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,7 @@ import { ElMessage } from 'element-plus'
 import 'element-plus/dist/index.css'
 // 引入pinia数据
 import { useUserStore } from "@/stores/user";
+import router from "@/router";
 
 const instance = axios.create({
   // 基地址
@@ -17,7 +18,6 @@ const instance = axios.create({
 // axios添加请求拦截器
 instance.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
-
   // 请求拦截器携带token
   const userStore = useUserStore()
   const token = userStore.userInfo.token
@@ -39,10 +39,23 @@ instance.interceptors.response.use(function (response) {
 }, function (error) {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
+
+  // 请求拦截器携带token
+  const userStore = useUserStore()
+
   ElMessage({
     type: "warning",
     message: error.response.data.message || '请求失败',
   })
+  // 如果401token失效过期
+  // 1.清除本地用户数据
+  // 2.跳转到登录页
+  if (error.response.status === 401) {
+    // 清除本地用户数据
+    userStore.clearUserInfo()
+    // 跳转到登录页
+    router.replace('/login')
+  }
   return Promise.reject(error);
 });
 
